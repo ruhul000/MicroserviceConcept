0b34949aeaed611aa36589ebde91d3470f85c8d8
Ruhul <ruhul.ewu00@gmail.com>
2023-11-29 17:25:37 +0600

    Added CommandsService Project

---
+++ CommandsService/Controllers/PlatformsControllers.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInBoundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+        
+    }
+}
