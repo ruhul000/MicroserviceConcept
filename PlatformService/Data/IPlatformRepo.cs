72d12c24aafb4e832970ce89fc5e2b06f2a2372c
Ruhul <ruhul.ewu00@gmail.com>
2023-11-29 14:58:02 +0600

    Added PlatformService

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetallPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
